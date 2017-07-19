5375da576b37dfc3b7381ea0aa50122507861a38
Sakref00unholy <phillipmayland@gmail.com>
2017-07-19 15:21:39 +0200

    second commit
    har tilføjet permissions og har ændret create metoden til fremtidige
    classer.

--- app/src/main/java/phillipmayland/camerafunctionv2/CameraMainActivity.java
+++ app/src/main/java/phillipmayland/camerafunctionv2/CameraMainActivity.java
@@ -1,29 +1,45 @@
 package phillipmayland.camerafunctionv2;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class CameraMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_main);
+        setContentView(R.layout.activity_add_image);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        int Permission_All = 1;
+
+        String[] Permissions = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.MEDIA_CONTENT_CONTROL};
+
+        if(!hasPermissions(this, Permissions)) {
+            ActivityCompat.requestPermissions(this, Permissions, Permission_All);
+        }
+        ivImage  = (ImageView) findViewById(R.id.ivImage);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show(); */
+
+
+                SelectImage();
+
             }
         });
     }

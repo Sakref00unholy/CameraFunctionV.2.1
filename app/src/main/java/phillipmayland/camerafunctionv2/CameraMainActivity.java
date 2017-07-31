19ba07766234987951fbfab5f379b189a1a1337d
Sakref00unholy <phillipmayland@gmail.com>
2017-07-31 15:20:16 +0200

    4 commit har tilføjet et par  kommentarer samt tilføjet en ny class til tilføjelsen senere af billederne.

--- app/src/main/java/phillipmayland/camerafunctionv2/CameraMainActivity.java
+++ app/src/main/java/phillipmayland/camerafunctionv2/CameraMainActivity.java
@@ -1,10 +1,20 @@
 package phillipmayland.camerafunctionv2;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -43,11 +53,61 @@ public class CameraMainActivity extends AppCompatActivity {
             }
         });
     }
+    private void SelectImage() {
+
+        final CharSequence[] items={"Camera", "Gallery", "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddImageActivity.this);
+        builder.setTitle("add Image");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(items[i].equals("Camera")){
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+
+                }else if (items[i].equals("Gallery")){
+
+                    Intent intent = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+
+
+
+                } else if (items[i].equals("Cancel")){
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode== Activity.RESULT_OK) {
+
+            if (requestCode==REQUEST_CAMERA) {
+
+                Bundle bundle = data.getExtras();
+                final Bitmap bmp = (Bitmap) bundle.get("data");
+                ivImage.setImageBitmap(bmp);
+
+
+            } else if (requestCode==SELECT_FILE) {
+
+                Uri selectedImageUri = data.getData();
+                ivImage.setImageURI(selectedImageUri);
+
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_camera_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_image, menu);
         return true;
     }
 
@@ -64,5 +124,17 @@ public class CameraMainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public static boolean hasPermissions (Context context, String... permissions) {
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && context !=null && permissions!=null){
+            for(String permission: permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission)!= PackageManager.PERMISSION_GRANTED) {
+                    return false;
+
+                }
+            }
+        }
+        return true;
     }
 }
